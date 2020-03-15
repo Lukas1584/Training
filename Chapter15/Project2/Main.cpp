0a589e42bec8750c713536facf34a4a0eb99ca14
Lukas Sergey <1584lukas@gmail.com>
2020-03-15 14:31:16 +0300

    Chapter 15 task 4
    Постройте графики функций sin ( ) , cos ( ) , sin (х) + cos ( х ) и sin ( х ) *sin (х) +cos (х) * cos ( х ) на одном рисунке. Изобразите оси и метки.

---
+++ Chapter15/Project2/Main.cpp
@@ -0,0 +1,62 @@
+#include <Simple_window.h>
+#include "Graph2.h"
+#include <iostream>
+
+
+using namespace Graph_lib;
+int main()
+try
+{
+    const int xmax = 840;      // window size
+    const int ymax = 440;
+
+    const int x_orig = xmax / 2; // position of (0,0) is center of window
+    const int y_orig = ymax / 2;
+    const Point orig(x_orig, y_orig);
+
+    const int r_min = -10;     // range [-10:10] 
+    const int r_max = 10;
+
+    const int n_points = 400;  // number of points used in range
+
+    const int x_scale = 40;    // scaling factors
+    const int y_scale = 100;
+
+    Simple_window win(Point(100, 100), xmax, ymax, "Chapter 15 task 4");
+
+    const int xlength = xmax - 40;   // make the axis a bit smaller than the window
+    const int ylength = ymax - 40;
+
+    Axis x(Axis::x, Point(20, y_orig), xlength, xlength / x_scale, "one notch == 1 rad");
+    Axis y(Axis::y, Point(x_orig, ylength + 20), ylength, 5*ylength / y_scale, "one notch == 0.2");
+    win.attach(x);
+    win.attach(y);
+    x.set_color(Color::black);
+    y.set_color(Color::black);
+
+    Function sin_(sin, r_min, r_max, orig, 200, x_scale, y_scale);
+    sin_.set_color(Color::blue);
+    win.attach(sin_);
+
+    Function cos_(cos, r_min, r_max, orig, 200, x_scale, y_scale);
+    cos_.set_color(Color::red);
+    win.attach(cos_);
+
+    Function c_s{ [](double x) {return sin(x) + cos(x); }, r_min, r_max, orig, 200, x_scale, y_scale };
+    c_s.set_color(Color::dark_blue);
+    win.attach(c_s);
+
+    Function cs2{ [](double x) {return sin(x)*sin(x) + cos(x)*cos(x); }, r_min, r_max, orig, 200, x_scale, y_scale };
+    cs2.set_color(Color::dark_green);
+    win.attach(cs2);
+
+    win.wait_for_button();
+}
+catch (const exception & e) {
+    std::cout << e.what();
+    return 1;
+}
+catch (...) {
+    
+    return 2;
+}
