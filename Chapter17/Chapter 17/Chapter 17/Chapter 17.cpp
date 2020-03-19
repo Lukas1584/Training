74e18552f853dcd749ed5efb259e3d7f7f88ea23
Lukas Sergey <1584lukas@gmail.com>
2020-03-19 23:17:36 +0300

    Chapter 17 task 3 upgrade

--- Chapter17/Chapter 17/Chapter 17/Chapter 17.cpp
+++ Chapter17/Chapter 17/Chapter 17/Chapter 17.cpp
@@ -2,15 +2,15 @@
 
 void to_lower(char* s)
 {
-    for (char* ptr = s;*ptr!=0;ptr++)
-         if (*ptr >= 'A' && *ptr <= 'Z')
-            *ptr += 'a' - 'A';       
- }
+    for (; *s; s++)
+        if (*s >= 'A' && *s <= 'Z')
+            *s += 'a' - 'A';
+}
 
 int size_s(const char* s)
 {
     int count = 0;
-    for (const char* ptr = s; *ptr != 0 ; ptr++)
+    for(; *s; s++)
         count++;
     return count;
 }
@@ -19,12 +19,10 @@ char* strdup1(const char* s)
 {
     int sz = size_s(s);
     char* str = new char[sz];
-    for (int i=0; i< sz;i++)
+    for (int i = 0; i <= sz; i++)
         str[i] = s[i];
-    str[sz] = 0;
     return str;
 }
-
 
 int main()
 {
@@ -32,8 +30,11 @@ int main()
     char str[200];
     std::cin >> str;
     to_lower(str);
-    std::cout << "In lower case:" << std::endl << str << std::endl << 
-        "New string:" <<std::endl<<strdup1(str);
+    std::cout << "In lower case:" << std::endl 
+        << str << std::endl
+        << "New string:" <<std::endl
+        << strdup1(str);
+    
 }
 
 
