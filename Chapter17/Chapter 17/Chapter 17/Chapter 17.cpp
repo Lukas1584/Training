a22fb99ca7620d5cb71fb48118a1847ece223a3b
Lukas Sergey <1584lukas@gmail.com>
2020-03-19 17:00:41 +0300

    Chapter 17 task 3
    Напишите функцию void to_lower ( char* s ) , заменяющую все прописные символы в С-строке s их строчными эквивалентами . Например, строка "Hel l o , World ! " должна принять вид "hel lo , world ! " . Не используйте стандартные библиотечные функции. С-строка представляет собой массив символов, который завершается нулем, поэтому если вы обнаружите символ О , то это значит, что вы находитесь в конце массива.

---
+++ Chapter17/Chapter 17/Chapter 17/Chapter 17.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+void to_lower(char* s)
+{
+    char* ptr = s;
+    for (char* ptr = s;*ptr!=0;ptr++)
+         if (*ptr >= 'A' && *ptr <= 'Z')
+            *ptr += 'a' - 'A';       
+ }
+
+int main()
+{
+    std::cout << "Please, input text (max 200 symbols)\n";
+    char str[200];
+    std::cin >> str;
+    to_lower(str);
+    std::cout << "In lower case:" << std::endl << str;
+}
+
+
