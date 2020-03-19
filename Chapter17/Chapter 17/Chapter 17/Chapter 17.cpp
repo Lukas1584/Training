13b9073d5b32b3f5be032f49009d6824bc4f44b4
Lukas Sergey <1584lukas@gmail.com>
2020-03-19 18:20:12 +0300

    Chapter 17 task 4
    Напишите функцию char* strdup(cons t char* ) , копирующую С-строку в выделенную в динамической памяти строку. Не используйте стандартные библиотечные функции .

--- Chapter17/Chapter 17/Chapter 17/Chapter 17.cpp
+++ Chapter17/Chapter 17/Chapter 17/Chapter 17.cpp
@@ -2,11 +2,29 @@
 
 void to_lower(char* s)
 {
-    char* ptr = s;
     for (char* ptr = s;*ptr!=0;ptr++)
          if (*ptr >= 'A' && *ptr <= 'Z')
             *ptr += 'a' - 'A';       
  }
+
+int size_s(const char* s)
+{
+    int count = 0;
+    for (const char* ptr = s; *ptr != 0 ; ptr++)
+        count++;
+    return count;
+}
+
+char* strdup1(const char* s)
+{
+    int sz = size_s(s);
+    char* str = new char[sz];
+    for (int i=0; i< sz;i++)
+        str[i] = s[i];
+    str[sz] = 0;
+    return str;
+}
+
 
 int main()
 {
@@ -14,7 +32,8 @@ int main()
     char str[200];
     std::cin >> str;
     to_lower(str);
-    std::cout << "In lower case:" << std::endl << str;
+    std::cout << "In lower case:" << std::endl << str << std::endl << 
+        "New string:" <<std::endl<<strdup1(str);
 }
 
 
