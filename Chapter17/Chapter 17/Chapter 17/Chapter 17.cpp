868c319127dbbc3c988b53e39531f637917a80e3
Lukas Sergey <1584lukas@gmail.com>
2020-03-20 19:27:55 +0300

    Chapter 17 task 5
    Напишите функцию char* findx ( const char* s , const char* х) , находящую
    первое вхождение С-строки х в строку s .

--- Chapter17/Chapter 17/Chapter 17/Chapter 17.cpp
+++ Chapter17/Chapter 17/Chapter 17/Chapter 17.cpp
@@ -17,23 +17,62 @@ int size_s(const char* s)
 
 char* strdup1(const char* s)
 {
+    char* str = new char[size_s(s)];
+    while(*s)
+        *(str++) = *(s++);
+    //*(str+1) = '\0';
+    str = str-(size_s(s) - 1)* sizeof(char);
+    std::cout << *str;
+    return str;
+}
+
+char* strdup2(const char* s)
+{
     int sz = size_s(s);
     char* str = new char[sz];
     for (int i = 0; i <= sz; i++)
         str[i] = s[i];
+    
     return str;
 }
 
+const char* findx(const char* s, const char* x)
+{
+    while (*s)
+    {
+        if(*s==*x)
+        {
+            const char* tmpx = x;
+            const char* tmps = s;
+            while (*tmpx)
+            {
+                if (*tmps == *tmpx)
+                {
+                    tmps++; tmpx++;
+                }
+                else break;
+            }
+            if (!(*tmpx)) return s;
+        }
+        s++;
+    }
+    return s;
+}
+
+
 int main()
 {
-    std::cout << "Please, input text (max 200 symbols)\n";
+    /*std::cout << "Please, input text (max 200 symbols)\n";
     char str[200];
     std::cin >> str;
     to_lower(str);
     std::cout << "In lower case:" << std::endl 
         << str << std::endl
         << "New string:" <<std::endl
-        << strdup1(str);
+        << strdup1(str);*/
+    const char* str1 = "Hello world";
+    const char* str2 = "lo";
+    std::cout << *(findx(str1,str2));
     
 }
 
