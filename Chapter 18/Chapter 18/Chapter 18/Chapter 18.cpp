fdd43ed261e2d4a6b7edd89d46006f0b24440dd6
Lukas Sergey <1584lukas@gmail.com>
2020-03-21 17:54:20 +0300

    Chapter 18 task 1
    Напишите функцию char* strdup (const char* ), копирующую С-строку в динамическую память, которую она выделяет для этой копии строки. Не используйте никаких стандартных функций. Не используйте индексирование, вместо него применяйте оператор разыменования* ·

---
+++ Chapter 18/Chapter 18/Chapter 18/Chapter 18.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int str_size(const char* s)
+{
+    int count = 0;
+    for(;*s;s++)
+        count++;
+    return count;
+}
+
+void str_copy(char *str, const char* s)
+{
+    for (; *s; str++, s++)
+        *str = *s;
+    *str++ = '\0';
+}
+
+char* strdup1(const char* s)
+{
+    int size = str_size(s);
+    char* str = new char[size];
+    str_copy(str, s);
+    return str;
+}
+
+int main()
+{   
+    const char* string = "Hello, world!";
+    std::cout << string << std::endl << strdup1(string);
+}
