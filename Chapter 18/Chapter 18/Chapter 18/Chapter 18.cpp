f5cb14ff5b960f6d58093b57b9d1f27933924821
Lukas Sergey <1584lukas@gmail.com>
2020-03-21 19:22:02 +0300

    Chapter 18 task 3
    Напишите функцию int strcmp (const char* s1, const char* s2 ), сравнивающую две С-строки . Если в лексикографическом порядке строка s1 меньше строки s2, функция должна возвращать отрицательное число, если строки совпадают - нуль, а если строка s1 больше строки s2 - положительное число. Не используйте никаких стандартных функций . Не используйте индексирование, вместо него
     применяйте оператор разыменования * .

--- Chapter 18/Chapter 18/Chapter 18/Chapter 18.cpp
+++ Chapter 18/Chapter 18/Chapter 18/Chapter 18.cpp
@@ -23,8 +23,40 @@ char* strdup1(const char* s)
     return str;
 }
 
+void str_to_lower(char* s)
+{
+    for (; *s; s++)
+        if (*s >= 'A' && *s <= 'Z')
+            *s += 'a' - 'A';
+}
+
+int strcmp(const char* s1, const char* s2)
+{
+    int sz_s1 = str_size(s1);
+    int sz_s2 = str_size(s2);
+    int min;
+    (sz_s1 < sz_s2) ? min = sz_s1 : min = sz_s2;
+    char* s1_low = strdup1(s1);
+    char* s2_low = strdup1(s2);
+    str_to_lower(s1_low);
+    str_to_lower(s2_low);
+    for (int i=0; i<=min; i++,s1_low++,s2_low++)
+    {
+        if (*s1_low < *s2_low)
+            return -1;
+        if (*s1_low > *s2_low)
+            return 1;
+    }
+    if (sz_s1 == sz_s2) return 0;
+    if (sz_s1 < sz_s2) return -1;
+    else return 1;
+}
+
 int main()
 {   
     const char* string = "Hello, world!";
     std::cout << string << std::endl << strdup1(string);
+    char s1 []= "Hello";
+    char s2 []= "hello";
+    std::cout << std::endl << strcmp(s1,s2);
 }
