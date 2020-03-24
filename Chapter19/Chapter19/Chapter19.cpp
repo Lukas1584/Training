fe96e1ecc8c24a11126c747f73d036c79b6d60c2
Lukas Sergey <1584lukas@gmail.com>
2020-03-24 20:23:53 +0300

    Chapter 19 task 1
    Напишите шаблонную функцию f(), складывающую элементы одного вектора vector<T> с элементами другого такого вектора; например, f(v1, v2 ) должна выполнять суммирование vl1[i]+=v2[i] для каждого элемента v1.

---
+++ Chapter19/Chapter19/Chapter19.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+
+
+template <typename T>
+void f(std::vector<T>& v1, std::vector<T>& v2)
+{
+	if (v1.size() != v2.size()) throw std::runtime_error("vector sizes are not equal");
+	for (int i = 0; i < v1.size(); i++)
+	{
+		v1.at(i) += v2.at(i);
+	}
+}
+
+template <typename T>
+void print(const std::vector<T> v)
+{
+	for (int i = 0; i < v.size(); i++)
+		std::cout << v.at(i) << std::endl;
+}
+
+int main()
+{
+	try
+	{
+		std::vector<int> tmp1{ 1,2,3 };
+		std::vector<int> tmp2{ 3,2,1 };
+		f(tmp1, tmp2);
+		print(tmp1);
+		std::vector<double> tmp3{ 1.1,2.2,3.3 };
+		std::vector<double> tmp4{ 3.3,2.2,1.1 };
+		f(tmp3, tmp4);
+		print(tmp3);
+		std::vector<std::string> tmp5{ "Hello, ", "To be " };
+		std::vector<std::string> tmp6{ "World!", "or not to be." };
+		f(tmp5, tmp6);
+		print(tmp5);
+		
+	}
+	catch (const std::exception & e) 
+	{
+		std::cout << " a standard exception was caught, with message '"
+			<< e.what() << "'\n";
+	}
+
+}
+
