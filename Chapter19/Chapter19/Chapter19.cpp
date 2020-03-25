ae64f0814639f8bd3984965ea993b587810ba370
Lukas Sergey <1584lukas@gmail.com>
2020-03-25 19:45:42 +0300

    Chapter 19 task 2
    Напишите шаблонную функцию. получающую в качестве арrументов объекты типов vector<T> vt и vector<U> vu и возвращающую сумму всех произведений vt [i] *vu [ i ] .

--- Chapter19/Chapter19/Chapter19.cpp
+++ Chapter19/Chapter19/Chapter19.cpp
@@ -3,7 +3,7 @@
 
 
 template <typename T>
-void f(std::vector<T>& v1, std::vector<T>& v2)
+void f(std::vector<T>& v1, const std::vector<T>& v2)
 {
 	if (v1.size() != v2.size()) throw std::runtime_error("vector sizes are not equal");
 	for (int i = 0; i < v1.size(); i++)
@@ -13,11 +13,22 @@ void f(std::vector<T>& v1, std::vector<T>& v2)
 }
 
 template <typename T>
-void print(const std::vector<T> v)
+void print(const std::vector<T>& v)
 {
 	for (int i = 0; i < v.size(); i++)
 		std::cout << v.at(i) << std::endl;
 }
+
+template <typename T, typename U>
+double task2(const std::vector<T>& vt, const std::vector<U>& vu)
+{
+	double res = 0;
+	if (vt.size() != vu.size()) throw std::runtime_error("vector sizes are not equal");
+	for (int i = 0; i < vt.size(); i++)
+		res += vt.at(i) * vu.at(i);
+	return res;
+}
+
 
 int main()
 {
@@ -35,6 +46,7 @@ int main()
 		std::vector<std::string> tmp6{ "World!", "or not to be." };
 		f(tmp5, tmp6);
 		print(tmp5);
+		std::cout << task2(tmp2, tmp4) << std::endl;
 		
 	}
 	catch (const std::exception & e) 
