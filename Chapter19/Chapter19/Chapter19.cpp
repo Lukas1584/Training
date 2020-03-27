ce30bca4e321b61033ad4257ae9ff0f2c079187b
Lukas Sergey <1584lukas@gmail.com>
2020-03-27 08:49:20 +0300

    Chapter 91 task 2
    Concept

--- Chapter19/Chapter19/Chapter19.cpp
+++ Chapter19/Chapter19/Chapter19.cpp
@@ -1,7 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
 
-
 template <typename T>
 void f(std::vector<T>& v1, const std::vector<T>& v2)
 {
@@ -19,7 +18,15 @@ void print(const std::vector<T>& v)
 		std::cout << v.at(i) << std::endl;
 }
 
+template <typename T, typename U> 
+concept amount_multiplication =
+requires(T t, U u)
+{
+	t* u + t * u;
+};
+
 template <typename T, typename U>
+requires amount_multiplication <T,U>
 double task2(const std::vector<T>& vt, const std::vector<U>& vu)
 {
 	double res = 0;
@@ -28,6 +35,37 @@ double task2(const std::vector<T>& vt, const std::vector<U>& vu)
 		res += vt.at(i) * vu.at(i);
 	return res;
 }
+
+/*
+class Int
+{
+	int i;
+public:
+	Int():i(0) {}
+	Int(int& ii) : i(ii) {}
+	Int(const int& ii) : i(ii) {}
+	Int(Int& ii) : i(ii.get()) {}
+	int get() const { return i; }
+	void set(int ii) { i = ii; }
+	void operator=(const int& ii) { i = ii; }
+	void operator=(const Int& ii) { i = ii.get(); }
+	//Int operator+(int& ii) { return Int (i+ii);}
+	int operator+(const Int& ii) { return i + ii.get(); }
+	int operator-(const int& ii) { return i - ii; }
+	int operator-(const Int& ii) { return i - ii.get(); }
+	int operator*(const int& ii) { return i * ii; }
+	int operator*(const Int& ii) { return i * ii.get(); }
+	int operator/(const int& ii) { if(!ii) return i + ii; }
+	int operator/(const Int& ii) { if (!ii.get()) return i + ii.get(); }
+	friend std::ostream& operator<<(std::ostream& os, Int ii);
+};
+std::ostream& operator <<(std::ostream& os, Int ii)
+{
+	os << ii.get() << std::endl;
+	return os;
+}
+*/
+
 
 
 int main()
@@ -42,12 +80,22 @@ int main()
 		std::vector<double> tmp4{ 3.3,2.2,1.1 };
 		f(tmp3, tmp4);
 		print(tmp3);
-		std::vector<std::string> tmp5{ "Hello, ", "To be " };
-		std::vector<std::string> tmp6{ "World!", "or not to be." };
-		f(tmp5, tmp6);
+		std::vector<std::string> tmp5{ "Hello, ", "To be " , "Blah "};
+		std::vector<std::string> tmp6{ "World!", "or not to be.", "blah" };
+		//f(tmp5, tmp6);
 		print(tmp5);
-		std::cout << task2(tmp2, tmp4) << std::endl;
-		
+		std::cout << task2(tmp2, tmp3) << std::endl;
+		std::cout << task2(tmp5, tmp6) << std::endl;
+		/*Int a;
+		std::cout << a;
+		a = 3;
+		std::cout << a;
+		Int b(a);
+		std::cout << b;
+		Int c;
+		c= a + b;
+		std::cout << c;
+		*/
 	}
 	catch (const std::exception & e) 
 	{
