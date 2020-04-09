1ff1f65b22bb9b0c5b41f8b04e3dbc1e35ff494e
Lukas Sergey <1584lukas@gmail.com>
2020-04-09 09:24:05 +0300

    Chapter 23 task 12
    Напишите программу на основе программы, выявляющей строки с датами (упр. 6) , найдите все даты и переведите их в формат ISO yyyy-mm-dd. Эта программа должна получать на вход файл и выводить результаты в выходной файл.
     идентичный входному, за одним исключением: даты в нем записаны в другом формате.

--- Chapter23/Chapter 23/Source.cpp
+++ Chapter23/Chapter 23/Source.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <fstream>
 #include<string>
 #include<regex>
-#include<map>
 
 std::vector<std::string> get_data(std::istream& is)
 {
@@ -14,53 +12,68 @@ std::vector<std::string> get_data(std::istream& is)
 	return text;
 }
 
-void print(const std::vector<std::string>& text)
+void print(const std::vector<std::string>& text, std::ostream& os)
 {
-	for (auto line : text)
-		std::cout << line << std::endl;
+	for (const auto& line : text)
+		os << line << std::endl;
 }
 
-
-
-void print(const std::map<std::string, std::string>& match)
+std::string replace(const std::string& date)
 {
-	for (auto line : match)
-		std::cout << line.first << std::endl << line.second << std::endl;
+	std::string result;
+	if (isdigit(date[2])) //2020x04x09
+		result = date.substr(0, 4) + '-' + date.substr(5, 2) + '-' + date.substr(8, 2); //2020-04-09
+	if (isdigit(date[7])) //09x04x2020
+		result = date.substr(6, 4) + '-' + date.substr(3, 2) + '-' + date.substr(0, 2); //2020-04-09
+	return result;
 }
 
-std::map<std::string, std::string> find_date(const std::vector<std::string>& text)
+std::string find_date_replace(const std::string& line)
 {
-	std::regex pattern { ("(\\d{4}-\\d{2}-\\d{2})|\\d{2}-\\d{2}-\\d{4}|\\d{2}[.]\\d{2}[.]\\d{4}|\\d{4}[.]\\d{2}[.]\\d{2}") };
-	
-	std::map<std::string, std::string> match;
-	for (auto line : text)
-	{
-		std::smatch matches;
-		if (std::regex_search(line, matches, pattern))
-			match.insert(std::make_pair(matches[0], line));
-	}
-			//std::cout<<"String number:"<< line+1 <<std::endl<<text.at(line)<<std::endl;
-	return match;
+	std::regex pattern{  R"((\d{4}[.]\d{2}[.]\d{2})|(\d{2}\D\d{2}\D\d{4}))" };
+	std::smatch matches;
+	std::string result=line;
+	while (std::regex_search(result, matches, pattern))
+		result = matches.prefix().str()+ replace(matches[0])+matches.suffix().str();
+	return result;
 }
 
+std::vector<std::string> find_date_replace(const std::vector<std::string>& original)
+{
+	std::vector<std::string> replaced = original;
+	for (std::string& line : replaced)
+		line = find_date_replace(line);
+	return replaced;
+}
 
+std::ifstream open_read(const std::string& file_name)
+{
+	std::ifstream file{ file_name };
+	if (!file) throw std::runtime_error("File doesn't exist");
+	return file;
+}
+
+std::ofstream open_save(const std::string& file_name)
+{
+	std::ofstream file{ file_name };
+	if (!file) throw std::runtime_error("File doesn't exist");
+	return file;
+}
 
 int main()
 {
 	try
 	{
-		std::ifstream file{ "text.txt" };
-		if (!file)throw std::runtime_error("File doesn't exist");
-		std::vector<std::string> text = std::move(get_data(file));
-		print(text);
-		std::cout << "		Strings with date:" << std::endl;
-		print(find_date(text));
-
+		std::ifstream is{ open_read("text.txt") };
+		std::vector<std::string> text{ get_data(is) };
+		std::cout << "	Original file: " << std::endl;
+		print(text,std::cout);
+		std::cout << "	Modified file: " << std::endl;
+		std::ofstream os{ open_save("result.txt") };
+		print(find_date_replace(text),os);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << "Exception: " << e.what() << std::endl;
 	}
-
-
 }
