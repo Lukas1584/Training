9b9228d6e76062fc911b443dd7f7e592c99ee8ac
Lukas Sergey <1584lukas@gmail.com>
2020-04-11 11:39:43 +0300

    Chapter 24 task 12
    Реализуйте операторы
    Matrix<double> operator* (Мatrix<double,2>& ,Matrix<double>&) ;
    и
    Matrix<double, N> operator+ (Мatrix<double, N>& , Matrix<douЬle, N>&)
    При необходимости посмотрите их математические определения в учебниках.

--- Chapter24/Project1/Main.cpp
+++ Chapter24/Project1/Main.cpp
@@ -17,16 +17,41 @@ void tripple(int& arg)
 {
 	arg = arg * arg * arg;
 }
-*/
+
 
 //a.apply(f,a)
 void tripple(int& arg,const int& pow)
 {
 	arg = std::pow(arg,pow);
 }
+*/
+
+Numeric_lib::Matrix<double,2> operator*(const Numeric_lib::Matrix<double,2>& mtrx, const Numeric_lib::Matrix<double>& vec)
+{
+	if (mtrx.dim2() != vec.dim1())throw std::runtime_error("Wrong matrix sizes");
+	Numeric_lib::Matrix<double, 2> result=mtrx;
+	for (int i = 0; i < result.dim1(); i++)
+		for(int j=0;j<result.dim2();j++)
+			result[i][j] *= vec[j];
+	return result;
+}
+
+template <int N>
+Numeric_lib::Matrix<double,N> operator+(const Numeric_lib::Matrix<double, N>& left, const Numeric_lib::Matrix<double, N>& right)
+{
+	if(left.size()!=right.size()) throw std::runtime_error("Wrong matrix sizes");
+	Numeric_lib::Matrix<double, N> result=left;
+	const double* ptrRight = right.data();
+	double* ptrResult = result.data();
+	for (int i = 0; i < result.size(); i++, ptrRight++, ptrResult++)
+		*ptrResult +=  (*ptrRight);
+	return result;
+}
+
 
 int main()
 {
+	/*
 	Numeric_lib::Matrix<int, 2> m(3, 3);
 	for (int i = 0; i < m.dim1(); i++)
 	{
@@ -40,6 +65,22 @@ int main()
 	std::cout << m;
 	m.apply([](int& arg) {arg = arg * arg * arg; });
 	std::cout << m;
+	*/
+	try
+	{
+		Numeric_lib::Matrix<double> vec(3);
+		vec[0] = 1; vec[1] = 2; vec[2] = 3;
+		Numeric_lib::Matrix<double, 2> n(2, 3);
+		for (int i = 0; i < n.dim1(); i++)
+			for (int j = 0; j < n.dim2(); j++)
+				n[i][j] = i + j;
+		std::cout << n << std::endl << "Multiply" << std::endl << vec << std::endl << "Result:" << std::endl << n * vec;
+		std::cout << "n=" << std::endl << n << std::endl << "n+n+n=" << std::endl << n + n + n;
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "Exception:" << e.what();
+	}
 	return 0;
 }
 
