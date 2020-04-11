0136b0b8d565d77a7ead68c7b396a7392f4d45b9
Lukas Sergey <1584lukas@gmail.com>
2020-04-11 22:24:04 +0300

    Chapter 24
    All tasks

--- Chapter24/Project1/Main.cpp
+++ Chapter24/Project1/Main.cpp
@@ -4,30 +4,49 @@
 #include <random>
 #include <vector>
 
-class Tripple
+struct Tripple
 {
-	public:
 	void operator()(int& arg)
 	{
 		arg=arg * arg * arg;
 	}
 };
 
-//a.apply(f)
 /*
-void tripple(int& arg)
+void tripple(int& arg)  //a.apply(f)
 {
 	arg = arg * arg * arg;
 }
-
-
-//a.apply(f,a)
-void tripple(int& arg,const int& pow)
-{
-	arg = std::pow(arg,pow);
-}
 */
 
+void tripple(int& arg,const int& pow)  //a.apply(f,a)
+{
+	arg = (int)std::pow(arg,pow);
+}
+
+void fill_matrix(Numeric_lib::Matrix<int, 2>& m)
+{
+	for (int i = 0; i < m.dim1(); i++)
+		for (int j = 0; j < m.dim2(); j++)
+			m[i][j] = i + j;
+}
+
+void task1()
+{
+	Numeric_lib::Matrix<int, 2> m(3, 3);
+	fill_matrix(m);
+	std::cout << m;
+	m.apply(Tripple());
+	std::cout << m;
+	m.apply(tripple, 3);
+	std::cout << m;
+	m.apply([](int& arg) {arg = arg * arg * arg; });
+	std::cout << m;
+}
+
+
+
+//Matrix: n[][]*m[]
 Numeric_lib::Matrix<double,2> operator*(const Numeric_lib::Matrix<double,2>& mtrx, const Numeric_lib::Matrix<double>& vec)
 {
 	if (mtrx.dim2() != vec.dim1())throw std::runtime_error("Wrong matrix sizes");
@@ -38,6 +57,7 @@ Numeric_lib::Matrix<double,2> operator*(const Numeric_lib::Matrix<double,2>& mtr
 	return result;
 }
 
+//Matrix: n+m
 template <int N>
 Numeric_lib::Matrix<double,N> operator+(const Numeric_lib::Matrix<double, N>& left, const Numeric_lib::Matrix<double, N>& right)
 {
@@ -50,50 +70,57 @@ Numeric_lib::Matrix<double,N> operator+(const Numeric_lib::Matrix<double, N>& le
 	return result;
 }
 
+void fill(Numeric_lib::Matrix<double, 2>& n) 
+{
+	for (int i = 0; i < n.dim1(); i++)
+		for (int j = 0; j < n.dim2(); j++)
+			n[i][j] = i + j;
+}
+
+void task12() //Matrix: n[][]*m[], n+m
+{
+	Numeric_lib::Matrix<double> vec(3);
+	vec[0] = 1; 
+	vec[1] = 2; 
+	vec[2] = 3;
+	Numeric_lib::Matrix<double, 2> n(2, 3);
+	fill(n);
+	std::cout << n << std::endl << "Multiply" << std::endl 
+		<< vec << std::endl << "Result:" << std::endl << n * vec;
+
+	std::cout << "n=" << std::endl << n << std::endl 
+		<< "n+n+n=" << std::endl << n + n + n;
+}
+
+
+
+void task6() //random
+{
+	int d, n;
+	std::cout << "Input two integer values" << std::endl;
+	std::cin >> n >> d;
+	std::vector<int> vec(n + 1);
+	std::default_random_engine ran;
+	for (int i = 0; i < d; i++)
+		vec[std::uniform_int_distribution<>{ 0, n }(ran)] ++;
+	for (unsigned int i = 0; i < vec.size(); i++)
+		std::cout << i << ":" << vec[i] << std::endl;
+}
+
+
 
 int main()
-{
-	/*
-	Numeric_lib::Matrix<int, 2> m(3, 3);
-	for (int i = 0; i < m.dim1(); i++)
-	{
-		for (int j = 0; j < m.dim2(); j++)
-			m[i][j] = i + j;
-	}
-	std::cout << m;
-	m.apply(Tripple());
-	std::cout << m;
-	m.apply(tripple,3);
-	std::cout << m;
-	m.apply([](int& arg) {arg = arg * arg * arg; });
-	std::cout << m;
-	
+{	
 	try
 	{
-		Numeric_lib::Matrix<double> vec(3);
-		vec[0] = 1; vec[1] = 2; vec[2] = 3;
-		Numeric_lib::Matrix<double, 2> n(2, 3);
-		for (int i = 0; i < n.dim1(); i++)
-			for (int j = 0; j < n.dim2(); j++)
-				n[i][j] = i + j;
-		std::cout << n << std::endl << "Multiply" << std::endl << vec << std::endl << "Result:" << std::endl << n * vec;
-		std::cout << "n=" << std::endl << n << std::endl << "n+n+n=" << std::endl << n + n + n;
+		task1();
+		task6();
+		task12();
 	}
 	catch (const std::exception& e)
 	{
 		std::cout << "Exception:" << e.what();
 	}
-	*/
-
-	int d, n;
-	std::cout << "Input two integer values"<<std::endl;
-	std::cin >> n >> d;
-	std::vector<int> vec(n+1);
-	std::default_random_engine ran;
-	for (int i = 0; i < d; i++)
-		vec[std::uniform_int_distribution<>{ 0, n }(ran)] ++;
-	for (int i = 0; i < vec.size(); i++)
-		std::cout << i << ":" << vec[i] << std::endl;
 	return 0;
 }
 
