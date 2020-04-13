f0ef2aaf041fa2ee491f873efcf1e9aefe42b324
Lukas Sergey <1584lukas@gmail.com>
2020-04-13 17:35:23 +0300

    Chapter 25 task 3
    3. Проинициализируйте 32-битовое целое число со знаком указанной битовой последовательностью и выведите его на экран : все биты нулевые. все биты единичные, чередующиеся нули и единицы (крайний слева бит - единичный), чередующиеся нули и единицы (крайний слева бит - нулевой), шаблон 110011 0011 . . ., 0011001100 . . . . чередующиеся байты, состоящие только из единичных и только из нулевых битов, начиная с байта, состоящего из одних нулей. Повторите это упражнение с 32-битовым целым числом без знака.

---
+++ Chapter25/Project25-2/Main.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+void task3()
+{
+	int a = 0b0;
+	int b = 0b11111111111111111111111111111111;
+	int c = 0b01010101010101010101010101010101;
+	int d = 0b10101010101010101010101010101010;
+	int e = 0b11001100110011001100110011001100;
+	int f = 0b00110011001100110011001100110011;
+	int g = 0b00001111000011110000111100001111;
+	int h = 0b11110000111100001111000011110000;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << c << std::endl;
+	std::cout << d << std::endl;
+	std::cout << e << std::endl;
+	std::cout << f << std::endl;
+	std::cout << g << std::endl;
+	std::cout << h << std::endl;
+	std::cout << std::endl;
+	unsigned int ua = 0b0;
+	unsigned int ub = 0b11111111111111111111111111111111;
+	unsigned int uc = 0b01010101010101010101010101010101;
+	unsigned int ud = 0b10101010101010101010101010101010;
+	unsigned int ue = 0b11001100110011001100110011001100;
+	unsigned int uf = 0b00110011001100110011001100110011;
+	unsigned int ug = 0b00001111000011110000111100001111;
+	unsigned int uh = 0b11110000111100001111000011110000;
+	std::cout << ua << std::endl;
+	std::cout << ub << std::endl;
+	std::cout << uc << std::endl;
+	std::cout << ud << std::endl;
+	std::cout << ue << std::endl;
+	std::cout << uf << std::endl;
+	std::cout << ug << std::endl;
+	std::cout << uh << std::endl;
+}
+
+int main()
+{
+	task3();
+	return 0;
+}
+
