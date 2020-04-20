aac7727c0f1ba80d7f7f8abaff184272fbd2424a
Lukas Sergey <1584lukas@gmail.com>
2020-04-20 11:42:15 +0300

    Chapter 25 task 15
    With random vectors

--- Chapter25/Chapter 25-15/Main.cpp
+++ Chapter25/Chapter 25-15/Main.cpp
@@ -3,23 +3,42 @@
 #include<random>
 #include<chrono>
 #include<memory>
+#include<algorithm>
 
 const int MAX = 10000;
+std::vector<int> rand_bite(MAX);
+std::vector<int> rand_order(MAX);
 
 struct Soo
 {
 	int a[125];
 };
 
+void fill_rand_bite()
+{
+	std::default_random_engine ran;
+	for (int count = 0; count < rand_bite.size(); count++)
+		rand_bite[count]=(std::uniform_int_distribution<>{ 0, 250 }(ran));
+}
+
+void fill_rand_order()
+{
+	std::default_random_engine ran;
+	for (int count = 0; count < rand_order.size(); count++)
+	{
+		int n = std::uniform_int_distribution<>{ 0, MAX }(ran);
+		if (!(std::binary_search(rand_order.begin(), rand_order.end(), n)))
+			rand_order[count]=n;
+	}
+}
+
 void random()
 {
 	int* a[MAX];
-	std::default_random_engine ran;
 	auto t1 = std::chrono::system_clock::now();
 	for (int count = 0; count < MAX; count++ )
 	{
-		int n = std::uniform_int_distribution<>{ 0, 250 }(ran);
-		a[count] = new int(n * sizeof(int));
+		a[count] = new int(rand_bite[count] * sizeof(int));
 	}
 	auto t2 = std::chrono::system_clock::now();
 	std::cout << "1 - in order, 2 - random" << std::endl;
@@ -27,29 +46,21 @@ void random()
 	std::cin >> choice;
 
 	auto t3 = std::chrono::system_clock::now();
-
 	if (choice == '1')
 		for (int i = MAX - 1; i >= 0; i--)
 			delete a[i];
-
-	std::default_random_engine ran2;
 	if (choice == '2')
 	{
-		for (int count = MAX; count > 0; )
+		for (int count = MAX-1; count >= 0; )
 		{
-			int n = std::uniform_int_distribution<>{ 0, MAX - 1 }(ran2);
-			if (a[n] != nullptr)
-			{
-				delete a[n];
-				a[n] = nullptr;
-				count--;
-			}
+			delete a[rand_order[count]];
+			a[rand_order[count]] = nullptr;
+			count--;
 		}
 	}
 	auto t4 = std::chrono::system_clock::now();
 	std::cout << "Created: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << std::endl;
 	std::cout << "Deleted: " << std::chrono::duration_cast<std::chrono::milliseconds>(t4 - t3).count() << std::endl;
-	
 }
 
 void pool()
@@ -68,18 +79,13 @@ void pool()
 		for (int i = MAX - 1; i >= 0; i--)
 			delete b[i];
 	
-	std::default_random_engine ran;
 	if (choice == '2')
 	{
-		for (int count = MAX; count > 0; )
+		for (int count = MAX - 1; count >= 0; )
 		{
-			int n = std::uniform_int_distribution<>{ 0, MAX-1 }(ran);
-			if (b[n] != nullptr)
-			{
-				delete b[n];
-				b[n] = nullptr;
-				count--;
-			}
+			delete b[rand_order[count]];
+			b[rand_order[count]] = nullptr;
+			count--;
 		}
 	}
 	auto t4 = std::chrono::system_clock::now();
@@ -124,7 +130,6 @@ void vector()
 
 void pointer()
 {
-
 	std::unique_ptr<Soo> arr[MAX];
 	auto t1 = std::chrono::system_clock::now();
 	for (int count = 0; count < MAX; count++)
@@ -136,12 +141,12 @@ void pointer()
 	auto t3 = std::chrono::system_clock::now();
 	std::cout << "Created: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << std::endl;
 	std::cout << "Deleted: " << std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2).count() << std::endl;
-
 }
 
 int main()
 {
-	
+	fill_rand_bite();
+	fill_rand_order();
 	std::cout << "1-random, 2-pool, 3 - stack, 4 - vector, 5 - smart pointer" << std::endl;
 	char choice=0;
 	std::cin >> choice;
